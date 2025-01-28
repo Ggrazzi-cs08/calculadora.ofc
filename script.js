27dd48aa568167403820c34153f9bb7c64d21668
Graziela <160619358+Ggrazzi-cs08@users.noreply.github.com>
2025-01-28 08:13:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ document.getElementById('calculate').addEventListener('click', function() {
         return;
     }
 
-    const i = fee / 100; // taxa de juros mensal
-    const n = time; // número de parcelas
-    const pmt = (value * i) / (1 - Math.pow(1 + i, -n)); // fórmula da parcela fixa
+    const i = fee / 100; 
+    const n = time; 
+    const pmt = (value * i) / (1 - Math.pow(1 + i, -n)); 
 
     const totalPago = pmt * n;
 
-    // Exibindo o valor da parcela e o total pago
+
     document.getElementById('parcel').innerHTML = "Parcela: R$ " + pmt.toFixed(2);
     document.getElementById('total').innerHTML = "Total Pago: R$ " + totalPago.toFixed(2);
 });
